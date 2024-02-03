7ab41ab5237dbb837f1c5f548c1ee305f0353f1c
azurecurve <github@azurecurve.co.uk>
2024-02-03 12:42:31 +0000

    Release v1.3.3

--- azrcrv-events.php
+++ azrcrv-events.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * ------------------------------------------------------------------------------
- * Plugin Name: Events
- * Description: Announce holidays, events, achievements and notable historical figures in a widget.
- * Version: 1.3.3
- * Author: azurecurve
- * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
- * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/azrcrv-events/
- * Text Domain: events
- * Domain Path: /languages
+ * Plugin Name:		Events
+ * Description:		Announce holidays, events, achievements and notable historical figures in a widget.
+ * Version:			1.3.4
+ * Requires CP:		1.0
+ * Author:			azurecurve
+ * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/azrcrv-events/
+ * Donate link:		https://development.azurecurve.co.uk/support-development/
+ * Text Domain:		events
+ * Domain Path:		/languages
+ * License:			GPLv2 or later
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.html
  * ------------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
